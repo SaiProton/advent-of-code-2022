52f5b7ad4a40dc6ecfef4fd4963daead693e8215
SaiProton <shayanr1001@gmail.com>
2023-04-05 20:16:52 -0400

    feat: adds part2 of day1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -3,9 +3,11 @@ mod utils;
 type Calorie = u32;
 
 pub fn main() {
-    println!("{}", most_calories("data/day1/calories.txt"));
+    println!("{}", most_calories("data/day1/calories.txt", 1));
+    println!("{}", most_calories("data/day1/calories.txt", 3));
 }
 
+// Attempts to convert a string to a calorie.
 fn convert_to_calorie(line: &str) -> Calorie {
     line.parse::<Calorie>().map_or_else(
         |err| panic!("Could not convert line to calorie: {err}"),
@@ -13,8 +15,8 @@ fn convert_to_calorie(line: &str) -> Calorie {
     )
 }
 
-fn most_calories(filename: &str) -> Calorie {
-    let most_calories = utils::read_file(filename)
+fn most_calories(filename: &str, amount: usize) -> Calorie {
+    let mut elf_calories = utils::read_file(filename)
         .split("\n\n") // Splits into calorie groups.
         .map(|calorie_group| {
             calorie_group
@@ -22,13 +24,10 @@ fn most_calories(filename: &str) -> Calorie {
                 .map(convert_to_calorie) // Attempt convert each line to calorie.
                 .sum::<Calorie>() // Sums the group of calories.
         })
-        .max() // Finds the max of the summed calorie groups.
-        .map_or_else(
-            || panic!("Could not find maximum calorie count."),
-            |max| max,
-        );
+        .collect::<Vec<Calorie>>(); // Collects all summed calories into a Vector of elves.
 
-    most_calories
+    elf_calories.sort_unstable(); // Sorts the vector.
+    elf_calories.into_iter().rev().take(amount).sum() // Returns sum of largest (n) elves.
 }
 
 #[cfg(test)]
@@ -37,12 +36,13 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert_eq!(24_000, most_calories("data/day1/calories-ex.txt"));
-        assert_eq!(67_622, most_calories("data/day1/calories.txt"));
+        assert_eq!(24_000, most_calories("data/day1/calories-ex.txt", 1));
+        assert_eq!(67_622, most_calories("data/day1/calories.txt", 1));
     }
 
     #[test]
     fn part2() {
-        assert_eq!(1, 1);
+        assert_eq!(45_000, most_calories("data/day1/calories-ex.txt", 3));
+        assert_eq!(201_491, most_calories("data/day1/calories.txt", 3));
     }
 }
