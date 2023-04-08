bbec00410f13b6ad64cd55c87802b445247b9f31
SaiProton <shayanr1001@gmail.com>
2023-04-08 17:35:31 -0400

    feat: adds part2 of day4

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -9,8 +9,13 @@ type Pair<T> = (T, T);
 pub fn main() {
     println!("{}", pair_comparison(PAIRS_TEST, &range_contains));
     println!("{}", pair_comparison(PAIRS_REAL, &range_contains));
+    // println!("{}", pair_comparison(PAIRS_TEST, &range_overlaps));
+    println!("{}", pair_comparison(PAIRS_REAL, &range_overlaps));
 }
 
+/// Compares the pairs of elves to compute the amount the pass the comparison.
+/// * `file_contents` - The contents of the file.
+/// * `range_fn` - A function that compares the pair of elves.
 fn pair_comparison(file_contents: &str, range_fn: &dyn Fn(&Pair<Pair<u32>>) -> bool) -> u32 {
     file_contents
         .lines() // Splits the list into individual lines of assignments.
@@ -28,16 +33,16 @@ fn pair_comparison(file_contents: &str, range_fn: &dyn Fn(&Pair<Pair<u32>>) -> b
         .sum() // Returns total amount of elf pairs that pass 'range_fn'
 }
 
+/// Attempts to split a line by ',' to a pair of elves.
 fn to_pairs(line: &str) -> Pair<String> {
-    // Attempts to split the line by a comma.
     line.split_once(',').map_or_else(
         || panic!("Could not split line into pair, no ',' found."),
         |pair| (pair.0.to_owned(), pair.1.to_owned()),
     )
 }
 
+/// Attempts to split each elf into a pair of endpoints by '-'
 fn to_endpoint_pairs(elves: &Pair<String>) -> Pair<Pair<String>> {
-    // Attempts to split each elf into a pair of endpoints by '-'
     (
         elves.0.split_once('-').map_or_else(
             || panic!("Could not split of pair, no '-' found."),
@@ -50,12 +55,12 @@ fn to_endpoint_pairs(elves: &Pair<String>) -> Pair<Pair<String>> {
     )
 }
 
+/// Attempts to parse a pair of endpoints to a defnied type.
 fn parse_endpoints<T>(endpoints: &Pair<String>) -> Pair<T>
 where
     T: FromStr + Display,
     <T as FromStr>::Err: Display,
 {
-    // Attempts to parse a pair of endpoints to a defnied type.
     (
         endpoints.0.parse().map_or_else(
             |err| panic!("Could not convert first endpoint: {err}"),
@@ -68,14 +73,18 @@ where
     )
 }
 
+/// Checks if the range of `elf_1` encompasses `elf_2`,
+/// or if the range of `elf_2` encompasses `elf_1`.
 fn range_contains<T: PartialOrd>((elf_1, elf_2): &Pair<Pair<T>>) -> bool {
-    // Checks for if elf_1's range encompasses elf_2, or if elf_2's range encompasses elf_1's.
     (elf_1.0 <= elf_2.0) && (elf_2.1 <= elf_1.1) || (elf_2.0 <= elf_1.0 && elf_1.1 <= elf_2.1)
 }
 
-// fn range_overlaps<T: PartialOrd>((elf_1, elf_2): &Pair<Pair<T>>) -> bool {
-//     todo!();
-// }
+/// Checks for overlapping between `elf_1` and `elf_2` by
+/// checking if the start of `elf_1` is past the end of `elf_2`,
+/// and the end of `elf_2` is past the start o`elf_1`.
+fn range_overlaps<T: PartialOrd>((elf_1, elf_2): &Pair<Pair<T>>) -> bool {
+    (elf_1.1 >= elf_2.0) && (elf_2.1 >= elf_1.0)
+}
 
 #[cfg(test)]
 mod tests {
@@ -87,8 +96,9 @@ mod tests {
         assert_eq!(560, pair_comparison(PAIRS_REAL, &range_contains));
     }
 
-    // #[test]
-    // fn part2() {
-    //     todo!()
-    // }
+    #[test]
+    fn part2() {
+        assert_eq!(4, pair_comparison(PAIRS_TEST, &range_overlaps));
+        assert_eq!(839, pair_comparison(PAIRS_REAL, &range_overlaps));
+    }
 }
